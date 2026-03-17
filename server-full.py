65124bb5ee4673e27dde287067416715fb66a5d9
michaspringer <michaspringer@gmail.com>
2026-03-17 23:20:18 +0200

    Update server-full.py

--- server-full.py
+++ server-full.py
@@ -742,12 +742,12 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
             'educational': 'חינוכי'
         }
         
-        # For testing: 1 page only!
+        # Full book - 6 pages!
         pages_by_age = {
-            '0-2': '1',
-            '3-5': '1',
-            '6-8': '1',
-            '9-12': '1'
+            '0-2': '4',
+            '3-5': '6',
+            '6-8': '8',
+            '9-12': '10'
         }
         
         pages = pages_by_age.get(data.get('childAge', '3-5'), '1')
